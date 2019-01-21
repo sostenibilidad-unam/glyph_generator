906801583321513d94f1e15b5fc071193836665d
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-01-20 18:23:03 -0600

    some ideas for an API

--- hier-o-glyph/__init__.py
+++ hier-o-glyph/__init__.py
@@ -4,43 +4,73 @@ import numpy as np
 import pandas as pd
 
 """
-data1 = { "total": {"name" : "Vulnerabilidad",
-                 "value": 0.5},
-         "categories":[{"name":"Exposición",
-                        "value":0.6,
-                        "subcategories":[{"name":"Expocisión A","value":0.2},
-                                         {"name":"Expocisión B","value":0.4},
-                                         {"name":"Expocisión C","value":0.8},
-                                         #{"name":"e4","value":0.5}
-                                         ]},
-                      {"name":"Resiliencia",
-                       "value":0.9,
-                       "subcategories":[{"name":"Resiliencia A","value":1.0},
-                                      {"name":"Resiliencia B","value":0.9},
-                                      {"name":"Resiliencia C","value":0.8},
-                                      {"name":"Resiliencia D","value":1.0},
-                                      {"name":"Resiliencia E","value":0.8}
-                                      ]},
-                       {"name":"Susceptibilidad",
-                        "value":0.4,
-                        "subcategories":[{"name":"Susceptibilidad A","value":1},
-                                         {"name":"Susceptibilidad B","value":1},
-                                         {"name":"Susceptibilidad C","value":0.2},
-                                         {"name":"Susceptibilidad D","value":0.3}
-                                         ]},
+g = Glyph('Vulnerability', 0.2)
+
+resilience = Arc('Resilience', 0.3)
+resilience.dots.append(Dot('res A', 0.1))
+resilience.dots.append(Dot('res B', 0.3, pallete=rainbow))
+
+icon_dot = Dot('res C', 0.2)
+icon_dot.svg.add_group(some_svg_icon)
+
+resilience.dots.append(icon_dot)
+
+g.add_arc(resilience)
 
 
-                    ]}
-g = Glyph(df, bars=True)
+g.add_arc(Arc('Susceptibility', 0.1))
+g.arcs['Susceptibility'].add_dot('sus A', 0.1)
 
 with open('glyph.svg', 'w') as f:
     f.write(g.render())
     
 """
 
+
+
+"""
+An experiment with pandas dataframe
+
+data = [{'E': 0.1, 'E1': 0.2},
+        {'R': 0.3, 'R1': 0.2, 'R2': 0.3},
+        {'S': 0.2, 'S1': 0.3, 'S2': 0.4, 'S3': 0.6}]
+
+df = pd.DataFrame(data=data, index=['E', 'R', 'S'])
+
+df.set_index(['E', 'R', 'S'])
+
+g = Glyph(df, bars=True)
+
+"""
+
+
+
 class Glyph:
 
-    def __init__(self):
-        pass
-
+    def __init__(self, label, value):
+        self.label = label
+        self.value = value
     
+
+class Arc:
+
+    def __init__(self, label, value, dots=[]):
+        self.label = label
+        self.value = value
+        self.dots = dots
+
+    def to_svg(self):
+        pass
+    
+
+    def add_dot(dot):
+        """
+        Add a peripheral dot to arc
+        """
+
+
+class Dot:
+
+    def __init__(self, label, value):
+        self.label = label
+        self.value = value
